604474fde97251ef1c07e1bbb479a927fa5d4d13
Mathew Desjardins <desjardinsm@etsu.edu>
2022-04-05 18:41:56 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Car car = new Car();
+System.Console.WriteLine(car.IsMyCarOn());
+
+car.Power("key");
+System.Console.WriteLine(car.IsMyCarOn());
+car.Power("secret key is");
+System.Console.WriteLine(car.IsMyCarOn());
+
+
+//System.Console.WriteLine(Car.RoadCond("It is a beautiful day!".ToUpper()));
+
+//string name = "Mathew";
+//String name = new String("Mathew");
+
